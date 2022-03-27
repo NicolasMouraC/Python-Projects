e3f005b1f3d40488af2b7576324f5d2d7889ea06
Nicolas de Moura <97635109+NicolasMouraC@users.noreply.github.com>
2022-03-26 21:23:34 -0300

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,61 +1,43 @@
-from gettext import find
-import random
-word = ""
-def get_word():
-    words = '''ant baboon badger bat bear beaver camel cat clam cobra cougar
+import sys
+from random import choice
+class Hangman:
+    def __init__(self):
+        self.error_out_tries = "Ran out of Tries"
+        self.words = '''ant baboon badger bat bear beaver camel cat clam cobra cougar
        coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
        lion lizard llama mole monkey moose mouse mule newt otter owl panda
        parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep
        skunk sloth snake spider stork swan tiger toad trout turkey turtle
        weasel whale wolf wombat zebra'''
-    return (random.choice(words.split()))
-def find_index(letterGuessed,wordToBeGuessed, tabs):
-    indexes = []
-    index = wordToBeGuessed.find(letterGuessed)
-    indexes.append(str(index))
-    if index != wordToBeGuessed.rfind:
-        index2 = wordToBeGuessed.rfind(letterGuessed)
-        indexes.append(str(index2))
-    for item in indexes:
-        item = indexes[int(item) - 1]
-        tabs = tabs[int(item)] + letterGuessed + tabs[int(item) + 1:]
-    return tabs
-def play():
-    global word 
-    word = get_word().upper()
-    print(word)
-    tabs = "_" * len(word)
-    guessed_letter = []
-    guessed = False
-    tries = 6
-    print("HANGMAN")
-    print(tabs)
-    while tries > 0:
-        guess = str(input("Guess a letter: ").upper())
-        if len(guess) > 1:
-            print("Please type only one letter per turn")
-            while True:
-                guess = str(input("Guess a letter: "))
-                if len(guess) == 1:
-                    break
-        if guess in guessed_letter:
-            print(f"{guess} already guessed")
-            while True:
-                guess = str(input("Guess a letter: "))
-                if guess not in guessed_letter:
-                    break
-        if word.find(guess):
-            print(f"Correct!\nThere's letter {guess} in the word")
-            #letter_guessed = find_index(guess, word)
-            find_index(guess, word, tabs)
-            print(tabs)
-        else:
-            print(f"Wrong! there's no letter {guess} in the word")
-            #Hangman print
-        if():
-            pass
-        #Check the won condition    
-        guessed_letter.append(guessed)
-        tries -= 1
+        self.word = choice(self.words.split())
+        self.tabs = "_" * len(self.word)
+        self.tries = 6
+        self.correct_letters = ""#[char for char in self.word]
+    def play(self):
+        print("HANGMAN")
+        print(self.word)
+        print(self.tabs)
+        while True:
+            print(f"You Have: {self.tries} left")
+            self.letter = str(input("Type a letter: "))
+            if self.letter[0] in self.word:
+                print(f"{self.letter[0]} in word")
+                self.c_letter = self.letter[0]
+                for i in range(len(self.word)):
+                    if self.word[i] in self.c_letter:
+                        self.tabs = self.tabs[:i] + self.c_letter + self.tabs[i+1:]
+                print(self.tabs)
+                self.correct_letters += self.c_letter
+                if self.correct_letters == self.word:
+                    print("You Won!")
+                    sys.exit()
+            elif self.letter[0] not in self.word:
+                print(f"{self.letter[0]} not in word")
+                self.tries -= 1
+                if self.tries < 1:
+                    print("You ran out of tries")
+                    raise Exception(self.error_out_tries)
+
 if __name__ == "__main__":
-    play()
+    Play = Hangman()
+    Play.play()
